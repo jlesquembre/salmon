002d02855ea2ee4e688890922fc098d7770112fb
José Luis Lafuente <jl@lafuente.me>
2022-05-30 23:21:28 +0200

    Update to latest clj-nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     clj-nix = {
-      url = "github:jlesquembre/clj-nix/next";
+      url = "github:jlesquembre/clj-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
